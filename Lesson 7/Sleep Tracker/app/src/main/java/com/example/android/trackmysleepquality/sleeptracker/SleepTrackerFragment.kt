bffeb4eb77e16caa2b823d2c4c08cc28bf1950af
inFngNam <inf.ng.nam@gmail.com>
2021-04-23 02:10:06 +0700

    Lesson 7: Exercise 18

--- Lesson 7/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Lesson 7/Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -71,6 +72,9 @@ class SleepTrackerFragment : Fragment() {
                 viewModel.onShowSnackbarEventComplete()
             }
         })
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
 
         binding.lifecycleOwner = this
         binding.sleepTrackerViewModel = viewModel
